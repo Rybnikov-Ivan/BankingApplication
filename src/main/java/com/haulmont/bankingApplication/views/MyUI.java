eaefeff207d26c5f5f8d81cfb4ca5d8644b6df3a
ivan <rybnikovv12345@gmail.com>
2021-09-23 18:10:06 +0400

    Create Main UI + updating multiple dependencies

---
+++ src/main/java/com/haulmont/bankingApplication/views/MyUI.java
@@ -0,0 +1,74 @@
+package com.haulmont.bankingApplication.views;
+
+import com.vaadin.annotations.Theme;
+import com.vaadin.icons.VaadinIcons;
+import com.vaadin.navigator.View;
+import com.vaadin.navigator.ViewDisplay;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.spring.annotation.SpringUI;
+import com.vaadin.spring.annotation.SpringViewDisplay;
+import com.vaadin.ui.*;
+import com.vaadin.ui.themes.ValoTheme;
+
+import java.io.Serializable;
+
+@Theme("valo")
+@SpringUI
+@SpringViewDisplay
+public class MyUI extends UI implements ViewDisplay, Serializable {
+
+    final VerticalLayout root = new VerticalLayout();
+    final HorizontalLayout navigationBar = new HorizontalLayout();
+    public static HorizontalLayout buttonsLayout;
+    private Panel springViewDisplay;
+
+    @Override
+    protected void init(VaadinRequest request) {
+
+        buttonsLayout = new HorizontalLayout();
+        buttonsLayout.addComponents(
+                createNavigationButton("Home", ""),
+                createNavigationButton("Clients", "bank"),
+                createNavigationButton("Bank", "credits"),
+                createNavigationButton("Credit", "clients"),
+                createNavigationButton("Credit Registration", "creditOffer")
+        );
+
+        buttonsLayout.getComponent(0).setIcon(VaadinIcons.HOME);
+        buttonsLayout.getComponent(1).setIcon(VaadinIcons.USERS);
+        buttonsLayout.getComponent(2).setIcon(VaadinIcons.PIGGY_BANK_COIN);
+        buttonsLayout.getComponent(3).setIcon(VaadinIcons.CREDIT_CARD);
+        buttonsLayout.getComponent(4).setIcon(VaadinIcons.MONEY_DEPOSIT);
+
+        navigationBar.setWidth("100%");
+        navigationBar.addComponents(buttonsLayout);
+
+        springViewDisplay = new Panel();
+        springViewDisplay.setSizeFull();
+
+        root.setSizeFull();
+        setContent(root);
+        root.addComponents(navigationBar);
+        root.addComponent(springViewDisplay);
+        root.setExpandRatio(springViewDisplay, 1.0f);
+    }
+
+    private Button createNavigationButton(String caption, final String viewName) {
+        Button button = new Button(caption);
+        button.addStyleName(ValoTheme.BUTTON_LARGE);
+        button.addClickListener(event -> getUI().getNavigator().navigateTo(viewName));
+        return button;
+    }
+
+    public static void setStyleForButton(int indexOfCurrentPage) {
+        int countOfButtons = buttonsLayout.getComponentCount();
+        for(int i=1; i<countOfButtons; i++)
+            buttonsLayout.getComponent(i).removeStyleName(ValoTheme.BUTTON_BORDERLESS_COLORED);
+        buttonsLayout.getComponent(indexOfCurrentPage).addStyleName(ValoTheme.BUTTON_BORDERLESS_COLORED);
+    }
+
+    @Override
+    public void showView(View view) {
+        springViewDisplay.setContent((Component) view);
+    }
+}
---
+++ src/main/java/com/haulmont/bankingApplication/views/main/MainView.java
@@ -0,0 +1,53 @@
+package com.haulmont.bankingApplication.views.main;
+
+import com.haulmont.bankingApplication.views.MyUI;
+import com.vaadin.icons.VaadinIcons;
+import com.vaadin.navigator.View;
+import com.vaadin.navigator.ViewChangeListener;
+import com.vaadin.server.ExternalResource;
+import com.vaadin.server.FileResource;
+import com.vaadin.spring.annotation.SpringView;
+import com.vaadin.ui.*;
+import com.vaadin.ui.themes.ValoTheme;
+
+import javax.annotation.PostConstruct;
+import java.io.File;
+import java.io.Serializable;
+
+@SpringView(name = "")
+public class MainView extends VerticalLayout implements View, Serializable {
+
+    @PostConstruct
+    void init() {
+        FileResource bankFile = new FileResource(new File("src/main/resources/images/bank.jpg"));
+
+        VerticalLayout verticalLayout = new VerticalLayout();
+        HorizontalLayout horizontalLayout = new HorizontalLayout();
+        HorizontalLayout headerLayout = new HorizontalLayout();
+
+        Link linkDev = new Link("About the developer",
+                new ExternalResource("https://vk.com/rrrybnikov"));
+        linkDev.setTargetName("_blank");
+
+        headerLayout.addComponent(linkDev);
+        headerLayout.setComponentAlignment(linkDev, Alignment.MIDDLE_CENTER);
+
+        Image bank = new Image("", bankFile);
+        bank.setWidth("30%");
+        bank.setHeight("30%");
+
+        verticalLayout.addComponent(bank);
+        verticalLayout.setComponentAlignment(bank, Alignment.TOP_CENTER);
+
+        horizontalLayout.setWidth("100%");
+        horizontalLayout.addComponent(verticalLayout);
+
+        setDefaultComponentAlignment(Alignment.MIDDLE_CENTER);
+        addComponents(headerLayout, horizontalLayout);
+    }
+
+    @Override
+    public void enter(ViewChangeListener.ViewChangeEvent viewChangeEvent) {
+
+    }
+}
