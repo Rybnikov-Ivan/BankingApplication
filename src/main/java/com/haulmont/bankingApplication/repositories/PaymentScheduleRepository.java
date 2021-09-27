f8179591aae476de374960332555d18fe4854cdf
ivan <rybnikovv12345@gmail.com>
2021-09-27 21:05:52 +0400

    fix PaymentSchedule logic

--- src/main/java/com/haulmont/bankingApplication/repositories/PaymentScheduleRepository.java
+++ src/main/java/com/haulmont/bankingApplication/repositories/PaymentScheduleRepository.java
@@ -2,8 +2,10 @@ package com.haulmont.bankingApplication.repositories;
 
 import com.haulmont.bankingApplication.models.PaymentSchedule;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface PaymentScheduleRepository extends JpaRepository<PaymentSchedule, UUID> {
 }
--- src/main/java/com/haulmont/bankingApplication/services/PaymentScheduleService.java
+++ src/main/java/com/haulmont/bankingApplication/services/PaymentScheduleService.java
@@ -3,10 +3,12 @@ package com.haulmont.bankingApplication.services;
 import com.haulmont.bankingApplication.models.PaymentSchedule;
 import com.haulmont.bankingApplication.repositories.PaymentScheduleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.UUID;
 
+@Service
 public class PaymentScheduleService {
     @Autowired
     PaymentScheduleRepository paymentScheduleRepository;
