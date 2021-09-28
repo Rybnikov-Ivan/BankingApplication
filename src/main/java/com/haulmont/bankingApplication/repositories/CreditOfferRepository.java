3c01f739e80ff5611025b56dbe89c9adfd56bfb2
ivan <rybnikovv12345@gmail.com>
2021-09-28 15:35:44 +0400

    add annotations for method deleted

--- src/main/java/com/haulmont/bankingApplication/repositories/CreditOfferRepository.java
+++ src/main/java/com/haulmont/bankingApplication/repositories/CreditOfferRepository.java
@@ -2,10 +2,12 @@ package com.haulmont.bankingApplication.repositories;
 
 import com.haulmont.bankingApplication.models.CreditOffer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
 import java.util.List;
 import java.util.UUID;
 
@@ -15,6 +17,8 @@ public interface CreditOfferRepository extends JpaRepository<CreditOffer, Long> 
     @Query("select p from CreditOffer p where p.bankId =:bankID")
     List<CreditOffer> findAllOffersForClient(@Param("bankID") Long bankID);
 
+    @Transactional
+    @Modifying
     @Query("delete from CreditOffer p where p.bankId =:bankID")
     void deleteOffersForClient(@Param("bankID") Long bankID);
 }
